984d4b240b18c43475b9ce5088718d57f5d7255c
Sunnyplace25 <Sunnyplace.nico@gmail.com>
2026-05-17 16:16:55 +0900

    fix: force cache bust v50

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'taskpwa-v32';
+const CACHE = 'taskpwa-v50';
 const ASSETS = ['./style.css', './app.js', './manifest.json', './icon.svg', './icon.png',
   './bg.png', './bg2.png', './bg3.png', './logo.png',
   './chara_hinata.png', './chara_hayate.png', './chara_kouta.png'];
